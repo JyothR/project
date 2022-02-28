caedda3199ff5c01423a56f732f31a0b3471fd9a
Sudhanya Chatterjee <sudhanya@arctechinfo.com>
2022-02-28 09:39:36 +0530

    initialCommit

---
+++ Books.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class Books
+{
+    private:
+        int BookCode;
+        string Title;
+        string Publisher;
+        string Author;
+        bool IssuedStatus;
+};
---
+++ LibrarySystem.cpp
@@ -0,0 +1,9 @@
+#include "Books.h"
+#include "Customers.h"
+#include "Transactions.h"
+
+int main()
+{
+    
+    return 0;
+}
